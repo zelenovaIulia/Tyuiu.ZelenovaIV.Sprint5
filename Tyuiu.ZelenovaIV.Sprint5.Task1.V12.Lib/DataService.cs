972d47f9d6edc062a524a095d2a5cee0bfa2b8d8
zelenovaIulia <zelenova_yulya@internet.ru>
2026-01-03 17:34:39 +0500

    Task11

--- Tyuiu.ZelenovaIV.Sprint5.Task1.V12.Lib/DataService.cs
+++ Tyuiu.ZelenovaIV.Sprint5.Task1.V12.Lib/DataService.cs
@@ -1,37 +1,34 @@
-﻿using tyuiu.cources.programming.interfaces.Sprint5;
+﻿using System.IO;
+using tyuiu.cources.programming.interfaces.Sprint5;
 
 namespace Tyuiu.ZelenovaIV.Sprint5.Task1.V12.Lib
 {
     public class DataService : ISprint5Task1V12
     {
         public string SaveToFileTextData(int startValue, int stopValue)
-        {
+        { 
             string path = $@"{Path.GetTempPath()}OutPutFileTask1.txt";
+            FileInfo fileInfo = new FileInfo(path);
+            bool FileExists = fileInfo.Exists;
 
-            FileInfo fileinfo = new FileInfo(path);
-            bool fileExists = fileinfo.Exists;
-
-            if (fileExists)
+            if (FileExists)
             {
                 File.Delete(path);
             }
-
             double y;
-            string strY;
+        string strY;
             for (int x = startValue; x <= stopValue; x++)
             {
-                y = (5 * x + 2.5 / Math.Sin(x) - 2) + 2;
+                y = Math.Round((5 * x + 2.5) / (Math.Sin(x) - 2) + 2, 2);
                 strY = Convert.ToString(y);
-
                 if (x != stopValue)
                 {
                     File.AppendAllText(path, strY + Environment.NewLine);
                 }
-
                 else
-                {
-                    File.AppendAllText(path, strY);
-                }
+{
+    File.AppendAllText(path, strY);
+}
             }
             return path;
         }
