bc8c1e79b917e93552a8428c0d4e4a89fa8749e3
zelenovaIulia <zelenova_yulya@internet.ru>
2026-01-03 23:38:18 +0500

    Task 2

---
+++ Tyuiu.ZelenovaIV.Sprint5.Task2.V19.Test/DataServiceTest.cs
@@ -0,0 +1,14 @@
+﻿using Tyuiu.ZelenovaIV.Sprint5.Task2.V19.Lib;
+
+namespace Tyuiu.ZelenovaIV.Sprint5.Task2.V19.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            string path = @"C:\Users\user\source\repos\Tyuiu.ZelenovaIV.Sprint5\Tyuiu.ZelenovaIV.Sprint5.Task2.V19\bin\Debug\net8.0\OutPutFileTask2.csv";
+        }
+    }
+}
---
+++ Tyuiu.ZelenovaIV.Sprint5.Task2.V19/Program.cs
@@ -0,0 +1,50 @@
+﻿using Tyuiu.ZelenovaIV.Sprint5.Task2.V19.Lib;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+
+        DataService ds = new DataService();
+
+        Console.Title = "Спринт #5 | Выполнил: Зеленова Ю. В. | АСОиУб-25-1";
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #5                                                               *");
+        Console.WriteLine("* Тема:  Запись данных в текстовый файл                                   *");
+        Console.WriteLine("* Задание #2                                                              *");
+        Console.WriteLine("* Вариант #1                                                              *");
+        Console.WriteLine("* Выпонил: Зеленова Юлия Владимировна | АСОиУб-25-1                       *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("* Вычислить значение выражения при значениях от -5 до 5                   *");
+        Console.WriteLine("* и сохранить результат в текстовый файл OutPutFileTask1.txt              *");
+        Console.WriteLine("*                                                                         *");
+        Console.WriteLine("***************************************************************************");
+        int[,] mtrx = new int[3, 3] { {9, 2, 5},
+                                      {8, 8, 2},
+                                      {7, 4, 8} };
+        int rows = mtrx.GetUpperBound(0) + 1;
+        int columns = mtrx.Length / rows;
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Матрица:                                                                *");
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < columns; j++)
+            {
+                Console.Write($"{mtrx[i, j]} \t");
+            }
+
+            Console.WriteLine();
+        }
+
+        Console.WriteLine();
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+        string res = ds.SaveToFileTextData(mtrx);
+        Console.WriteLine("Файл: " + res);
+        Console.WriteLine("Создан!");
+        Console.ReadKey();
+    }
+}
